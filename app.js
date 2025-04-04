0f8fab04fb0ba8233936fd5f99d549f9e55ee3a3
Etim Israel <israeletim4@gmail.com>
2025-04-04 13:44:18 +0100

    adding animation effects to all elements

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const highlightMenu = () => {
   let scrollPos = window.scrollY;
 
   // To check for page number to know the px to put as used below (> 960...), we use the console below
-  console.log(scrollPos);
+  // console.log(scrollPos);
 
   // Adds 'highlight' class to my menu items
   if(window.innerWidth > 960 && scrollPos < 600) {
@@ -60,51 +60,25 @@ navLogo.addEventListener('click', hideMobileMenu);
 // Animations
 gsap.registerPlugin(ScrollTrigger)
 
-gsap.from('.animate-hero', {
+gsap.from('.animate-logo', {
+  ScrollTrigger: 'animate-hero',
   duration: 0.6,
   opacity: 0,
   y: -150,
   stagger: 0.3
 });
 
-gsap.from('.animate-about', {
-  ScrollTrigger: 'animate-about',
-  duration: 0.5,
-  opacity: 1,
-  x: -150,
-  stagger: 0.12
-});
-
-gsap.from('.animate-about', {
-  ScrollTrigger: 'animate-about',
-  duration: 1.2,
-  opacity: 0,
-  y: -200,
-});
-
-gsap.from('.animate-services', {
-  ScrollTrigger: 'animate-services',
-  duration: 1,
-  opacity: 0,
-  y: -150,
-  stagger: 0.3,
-  delay: 0.5
-});
-
-gsap.from('.animate-features', {
-  ScrollTrigger: 'animate-features',
-  duration: 1,
-  opacity: 0,
-  y: -150,
-  stagger: 0.1,
-  delay: 0.2
-});
-
-gsap.from('.animate-email', {
-  ScrollTrigger: 'animate-email',
-  duration: 0.8,
-  opacity: 0,
-  y: -150,
-  stagger: 0.25,
-  delay: 1
+gsap.utils.toArray('.animate-element').forEach(element => {
+  gsap.from(element, {
+    scrollTrigger: {
+      trigger: element,
+      start: 'top 80%', // Animation starts when the element is 80% from the top of the viewport
+      end: 'bottom 20%', // Animation ends when the element is 20% from the bottom of the viewport
+      scrub: true // Animation scrubs with the scroll
+    },
+    duration: 0.6,
+    opacity: 0,
+    y: -50,
+    stagger: 0.3
+  });
 });
